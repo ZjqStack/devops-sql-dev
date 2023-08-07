ec11120f39bff13fd049d2a53ad9fbe08327ccc2
kubernetes <11122020+zhangjq1231@user.noreply.gitee.com>
2023-08-07 14:58:14 +0800

    re

--- bytebase/Project1/prod/employee##v1##ddl##create_table.sql
+++ bytebase/Project1/prod/employee##v1##ddl##create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE timecards (
   `timecard_id` INT AUTO_INCREMENT PRIMARY KEY,
-  `emp_no1` INT NOT NULL,
+  `emp_no` INT NOT NULL,
   `date` DATE NOT NULL,
   `start_time` TIME NOT NULL,
   `end_time` TIME NOT NULL,
